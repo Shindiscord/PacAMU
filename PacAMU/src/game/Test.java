c3a2f97051babecc0261c29d569fc5149b068bbd
Shindiscord <anthony.gignac@gmail.com>
2019-11-26 19:02:13 +0100

    Update to Max-Antho

--- PacAMU/src/game/Test.java
+++ PacAMU/src/game/Test.java
@@ -3,27 +3,41 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import amuEngine.graphics.*;
+import game.map.Grid;
+import game.map.TileManager;
+
+import java.io.IOException;
+
 import amuEngine.*;
 
-class TileSetTest implements amuGameObject{
+/*class TileSetTest implements amuGameObject{
 	private Sprite s = new SingleSprite(new Image("/img/tilesetTest.png")
 			,546, 418, 1);
 	public Sprite getSprite() {return this.s;}
 	public void update(long time) {
 	}
-}
+}*/
 
 public class Test extends Application {
     public static void main(String[] args) {
-        launch(args);
+    	launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
+    	Grid grid = null;
+		try {
+			grid = new Grid("res/mapFiles/map1.txt", 17, 13);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	grid.print();
         primaryStage.setTitle("test1");
         GameRoom room = new GameRoom(40);
-        room.addObject(new TileSetTest());
-        room.addObject(new Student(64, 64, 640, 480));
-        room.start(primaryStage, 640, 480);
+        TileManager.placeTiles(room, grid, 32, 32);
+        //room.addObject(new TileSetTest())
+        room.addObject(new Student(32, 32, 32*17, 32*13, grid));
+        room.start(primaryStage, grid.getLargeur()*32,grid.getHauteur()*32);
     }
 }
---
+++ PacAMU/src/game/map/Grid.java
@@ -0,0 +1,101 @@
+package game.map;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.scene.input.KeyCode;
+
+public class Grid {
+
+	private File f;
+	private FileReader fr;
+	private BufferedReader br;
+	private char[][] map;
+	private int largeur;
+	private int hauteur;
+	
+	public int getHauteur() {
+		return this.hauteur;
+	}
+	
+	public int getLargeur() {
+		return this.largeur;
+	}
+	
+	
+	public char getTile(int x, int y) {
+		if(x >= this.largeur)
+			x = 0;
+		if(x <= -1)
+			x = this.largeur-1;
+		if(y >= this.hauteur)
+			y = 0;
+		if(y <= -1)
+			y = this.hauteur-1;
+		return this.map[x][y];
+	}
+	
+	public boolean isAWall(int x, int y) {
+		if(x < 0 || x >= this.largeur || y < 0 || y >= this.hauteur)
+			return false;
+		if(this.map[x][y] == 'm')
+			return true;
+		return false;
+	}
+	
+	public boolean nextIsAWall(int x, int y, KeyCode direction) {
+		switch(direction) {
+			case UP:
+				return isAWall(x, y-1);
+			case DOWN:
+				return isAWall(x, y+1);
+			case LEFT:
+				return isAWall(x-1, y);
+			case RIGHT:
+				return isAWall(x+1, y);
+			default:
+				System.out.println("Not a direction for a wall");
+				return false;
+		}
+	}
+	
+	public Grid(String address, int x, int y) throws IOException {
+		this.f = new File(address);
+		this.fr = new FileReader(this.f);
+		this.br = new BufferedReader(this.fr);
+		this.map = new char[x][y];
+		this.largeur = x;
+		this.hauteur = y;
+		
+		int c = 0; int i = 0; int j = 0;
+		
+		while((c = br.read()) != -1) {
+			if(Character.isWhitespace(c)) {
+				continue;
+			}
+			this.map[i][j] = (char) c; //conversion integer -> char
+			
+			i++;
+
+			if ( i >= x){
+				i = 0;
+				j++;
+				if(j >= y) {
+					break;
+				}
+			}
+		}
+	}
+	
+	public void print() {
+		for(int i = 0 ; i < hauteur ; i++) {
+			for(int j = 0 ; j < largeur ; j++) {
+				System.out.print(map[j][i] + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
